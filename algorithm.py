c8687e4b16cd2c7e19a6a1e6732885fbe7b33d0c
Tushar Maharanagit <tusharhero@sdf.org>
2023-03-15 23:35:28 +0530

    fix bugs in algorithms

--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,28 @@
+'''
+The GPLv3 License (GPLv3)
+
+Copyright (c) 2023 Tushar Maharana
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 from PIL import Image as im
 import random as ran
 
 
 def getfilename(
-    shape,
+    shape, var = 20
 ):  # select random files from dataset. (This is possible because they follow a specific naming rule.)
     filename = ""
     if shape == "circle":
@@ -19,11 +38,16 @@ def getfilename(
         filename = f"rectangles/({a},{b},{c},{d}).png"
     return filename
 
+def list_float2int(list):
+    intlist = []
+    for i in list:
+        intlist.append(int(i))
+    return intlist
 
 def genrandweight(size):
     weight = []
-    for i in size[0]:
-        weight[i] = ran.randint(-1, 1)
+    for i in range(size[0]*size[1]):
+        weight.append(ran.randint(-1, 1))
     return weight
 
 
@@ -35,14 +59,14 @@ def getfilecontent(filepath):
 
 def multiply(weight, image_list, size=(20, 20)):
     product = 0
-    for i in size[0]:
+    for i in range(size[0]*size[1]):
         product += weight[i] * image_list[i]
     return product
 
 
 def addition(weight, image_list, SubOrAdd=1, size=(20, 20)):
     sum = []
-    for i in size[0]:
+    for i in range(size[0]*size[1]):
         sum.append(weight[i] + image_list[i] * SubOrAdd)
     return sum
 
@@ -64,14 +88,15 @@ def train(
     # load weight from file or generate randomly
     if len(weightpath) == 0:
         weight = genrandweight(size)
+        weightpath = "weight"
     else:
         weight = eval(getfilecontent("weight"))
 
     correct_guesses = 0
 
     for i in range(enouchs):
-        shape = shapes[randrange(2)]
-        image_list = normalize(list(im.getdata(im.open(getfilename(shape)))))
+        shape = shapes[ran.randrange(2)]
+        image_list = normalize(list((im.open(getfilename(shape)).getdata())),1)
 
         product = multiply(weight, image_list)
 
@@ -89,13 +114,13 @@ def train(
 
         if i % 1000 == 0:
             weight_viz = im.new("RGB", size)
-            weight_viz.putdata(weight)
+            weight_viz.putdata(list_float2int(weight))
             weight_viz.save("weight.png")
 
             with open(weightpath, "w") as w:
                 w.write(str(weight))
 
-            with open(log, "a") as log:
-                log.write(f"{correct_guesses/i}; {i/enouchs * 100}%")
+            with open("log", "a") as log:
+                log.write(f"{correct_guesses/(i+1)}; {i/(enouchs+1) * 100}%")
 
         print(i)
